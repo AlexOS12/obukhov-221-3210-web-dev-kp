a3817a96bdc2727691c9269c8234bdbbaaf8a6eb
AlexOS12 <sluhach2016@gmail.com>
2024-06-06 19:49:41 +0300

    Создана связь с бд

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from flask_login import login_required, current_user
+from mysqldb import DBConnector
+from mysql.connector.errors import DatabaseError
 
 app = Flask(__name__)
 application = app
 app.config.from_pyfile("config.py")
+
+db_connector = DBConnector(app)
 
 from authorization import bp as authorization_bp, init_login_manager
 app.register_blueprint(authorization_bp)
--- app/authorization.py
+++ app/authorization.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, render_template, Blueprint, current_app, request, url_for
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+from app import db_connector
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -21,28 +22,38 @@ def init_login_manager(app):
     login_manager.user_loader(load_user)
 
 def load_user(user_id):
-    return User(1, "test", 1)
-    # query = "SELECT id, login, role_id FROM users WHERE id=%s"
+    query = "SELECT id, login, role_id FROM users WHERE id=%s"
 
-    # with db_connector.connect().cursor(named_tuple=True) as cursor:
+    with db_connector.connect().cursor(named_tuple=True) as cursor:
+        cursor.execute(query, (user_id, ))
+        user = cursor.fetchone()
 
-    #     cursor.execute(query, (user_id,))
-        
-    #     user = cursor.fetchone()
-
-    # if user:
-    #     return User(user_id, user.login, user.role_id)
+    if user:
+       return User(user_id, user.login, user.role_id)
+    return None
     
-    # return None
 
 @bp.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("auth.html")
     
-    login_user(User(1, "test", 1))
-    target_page = request.args.get("next", url_for("index"))
-    return redirect(target_page)
+    login = request.form.get("login", "")
+    password = request.form.get("pass", "")
+    remember = request.form.get("remember") == "on"
+
+    query = "SELECT id, login, role_id FROM users WHERE login=%s and pass_hash=SHA2(%s, 256)"
+
+    with db_connector.connect().cursor(named_tuple=True) as cursor:
+        cursor.execute(query, (login, password))
+        user = cursor.fetchone()
+
+    if user:
+        login_user(User(user.id, user.login, user.role_id), remember=remember)
+        target_page = request.args.get("next", url_for("index"))
+        return redirect(target_page)
+
+    return render_template("auth.html")
 
 @bp.route('/logout')
 def logout():
